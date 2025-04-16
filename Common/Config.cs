458e2dacc7652895d0b895ecfce06a41a59f27a6
student-a502 <student-a502@PC-A502-6>
2025-04-16 17:16:13 +0500

    Разработан класс Config, хранящий данные для подключения к БД Разработан класс Context для работы с БД

---
+++ Common/Config.cs
@@ -0,0 +1,7 @@
+﻿namespace PMSBackend.Common
+{
+    public class Config
+    {
+        public readonly static string ConnectionString = "Server=;Database=;Trusted_Connection=true;Encrypt=false";
+    }
+}
---
+++ Controllers/ApplicationContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PMSBackend.Common;
+using PMSBackend.Models;
+
+namespace PMSBackend.Controllers
+{
+    public class ApplicationContext :DbContext
+    {
+        public DbSet<Participation> Participations { get; set; }
+        public DbSet<Project> Projects {  get; set; }
+        public DbSet<Role> Roles { get; set; }
+        public DbSet<Status> Statuses { get; set; }
+        public DbSet<Subtask> Subtasks { get; set; }
+        public DbSet<Models.Task> Tasks { get; set; }
+        public DbSet<User> Users { get; set; }
+
+        public ApplicationContext() 
+        {
+            Database.EnsureCreated();
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(Config.ConnectionString);
+        }
+    }
+}
